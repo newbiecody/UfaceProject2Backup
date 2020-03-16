89e8ed12065a7566d97bfb0e9a794a6a51c4a6e7
Xiaochen Jia <cjasonjia@gmail.com>
2020-03-16 14:22:37 +0800

    Add email for student obj

--- Firebase/FirebaseManager.py
+++ Firebase/FirebaseManager.py
@@ -218,14 +218,17 @@ class Student:
         self.name = name
         self.facial_info = []
         self.attendance_records = {}
+        self.email = ""
 
     @classmethod
     def init_from_fb_dict(cls, student_dict):
         student = Student(student_dict["matric_num"], student_dict["name"])
         fb_facial_info = student_dict["facial_info"]
+        student.attendance_records = student_dict["attendance_records"]
+        if "email" in student_dict.keys():
+            student.email = student_dict["email"]
         for x in range(len(fb_facial_info)):
             student.facial_info.append(fb_facial_info[x]["value"])
-        student.attendance_records = student_dict["attendance_records"]
         return student
 
     def dict(self):
@@ -233,7 +236,8 @@ class Student:
         for x in range(len(self.facial_info)):
             fb_facial_info.append({"value": self.facial_info[x]})
         return {"matric_num": self.matric_num, "name": self.name,
-                "facial_info": fb_facial_info, "attendance_records": self.attendance_records}
+                "facial_info": fb_facial_info, "attendance_records": self.attendance_records,
+                "email": self.email}
 
 
 def test():
@@ -252,11 +256,11 @@ def test():
     set_student("cz3002", "sp1", {"matric_num": "0", "name": None,
                                   "facial_info": [{"value": [0, 1]}], "attendance_records": {}})
     print(get_student_obj("cz3002", "sp1", "0").dict() ==
-          {'matric_num': '0', 'name': None, 'facial_info': [{'value': [0, 1]}], 'attendance_records': {}})
+          {'matric_num': '0', 'name': None, 'facial_info': [{'value': [0, 1]}], 'attendance_records': {}, 'email': ''})
 
     create_student("cz3002", "sp1", "0", "Jason")
     print(get_student_obj("cz3002", "sp1", "0").dict() ==
-          {'matric_num': '0', 'name': 'Jason', 'facial_info': [], 'attendance_records': {}})
+          {'matric_num': '0', 'name': 'Jason', 'facial_info': [], 'attendance_records': {}, 'email': ''})
 
     set_attendance_record("cz3002", "sp1", "0", "20200313", True)
     print(get_attendance_record("cz3002", "sp1", "0", "20200313"))
@@ -272,14 +276,14 @@ def test():
                      [0, 0, 0, 0]])
     print(get_all_facial_info("cz3002", "sp1") == [[[0, 0, 1, 1], [1, 1, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]], []])
 
-    # del_student("cz3002", "sp1", "0")
-    # print(get_student_obj("cz3002", "sp1", "0") is None)
-    #
-    # del_module("cz3002", "sp1")
-    # print(get_module_dict("cz3002", "sp1") is None)
-    #
-    # del_course("cz3002")
-    # print(get_course_dict("cz3002") is None)
+    del_student("cz3002", "sp1", "0")
+    print(get_student_obj("cz3002", "sp1", "0") is None)
+
+    del_module("cz3002", "sp1")
+    print(get_module_dict("cz3002", "sp1") is None)
+
+    del_course("cz3002")
+    print(get_course_dict("cz3002") is None)
 
 
 # test()
